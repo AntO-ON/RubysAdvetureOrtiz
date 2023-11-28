722361752790cdcebb06ceef4ce75ac76152730a
AntO-ON <145067457+AntO-ON@users.noreply.github.com>
2023-11-27 22:52:31 -0800

    life this is what it is now
    ( ´･･)ﾉ(._.`)

--- RubysAdventureOrtiz/Assets/Scripts/RubyControler.cs
+++ RubysAdventureOrtiz/Assets/Scripts/RubyControler.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyControler : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int current health;
+    
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-    
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * Vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        
+        rigidbody2d.MovePosition(position);
+    }
+    void changeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth, amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
